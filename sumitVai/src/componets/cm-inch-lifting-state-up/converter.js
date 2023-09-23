35667794a4ca09971b33e32fe7b222f52199311d
Nayem <naeemislam13790@gamil.com>
2023-09-23 18:35:06 +0600

    'commited'

--- sumitVai/src/componets/cm-inch-lifting-state-up/converter.js
+++
@@ -1,9 +0,0 @@
-
-export function convert(feet,inch){
-    if(Number.isNaN(feet)){ // if input is't a number, it return a blank string
-        return ''
-    }
-    let fCM = feet*30.48
-    let iCM = inch*2.54
-    return iCM+fCM
-}
